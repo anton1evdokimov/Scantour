d7df9994a1d10e02210469f401bd184c86f8db2e
anton1evdokimov <anton1evdokimov@yandex.ru>
2020-04-07 20:48:48 +0300

    7thApril

--- js/menu.js
+++ js/menu.js
@@ -1,15 +1,27 @@
 var list = document.querySelectorAll('.left-menu__nav__location');
 for (var i = 0; i < list.length; i++) {
-    list[i].addEventListener('click', event => {
+    list[i].addEventListener('click', function(event) {
         var e = event.currentTarget;
         if (e.classList.contains('minus')) {
-            e.classList.replace('minus', 'plus');
+            e.classList.add('plus');
+            e.classList.remove('minus');
             e.querySelector('.left-menu__nav__submenu').style.display = 'none';
         } else {
-            e.classList.replace('plus', 'minus');
+            e.classList.add('minus');
+            e.classList.remove('plus');
             e.querySelector('.left-menu__nav__submenu').style.display = 'block';
         }
     })
 }
 
-//var event.target = document.querySevent.targetector('.left-menu__nav__location__header__icon');
+var nav = document.querySelector('.top-menu');
+nav.addEventListener('click', function (event) {
+    var t = event.target;
+    var subMenu = t.nextSibling;
+    if (t.classList.contains('top-menu-txt')) {
+        if (subMenu.style.display === 'none'){
+            subMenu.style.display = 'flex';
+        }
+        else subMenu.style.display = 'none';
+    }
+})
