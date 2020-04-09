94c0fb1e1561242f6b020aaaefe5d73ba26c2569
anton1evdokimov <anton1evdokimov@yandex.ru>
2020-04-09 11:27:44 +0300

    9thApril

--- js/menu.js
+++ js/menu.js
@@ -15,6 +15,7 @@ for (var i = 0; i < list.length; i++) {
 }
 
 var nav = document.querySelector('.top-menu');
+var list = nav.querySelectorAll('.top-sub-menu');
 nav.addEventListener('click', function (event) {
     var t = event.target;
     if (t.classList.contains('top-menu-txt')) {
@@ -30,8 +31,36 @@ nav.addEventListener('click', function (event) {
 nav.addEventListener('mouseleave', close);
 
 function close() {
-    var list = nav.querySelectorAll('.top-sub-menu');
     for (var i = 0; i < list.length; i++) {
         list[i].style.display = 'none';
     }
 }
+
+var dropDown = document.querySelector('#phone-drop-down');
+var phone = document.querySelector('#phone-block');
+
+function closeDropDown(){
+    dropDown.style.height = "";
+    dropDown.style.border = "none";
+}
+
+phone.addEventListener('mouseleave', closeDropDown);
+
+document.querySelector('#open-drop-down').addEventListener('click', function () {
+    if (dropDown.style.height === "") {
+        dropDown.style.height = "100px";
+        dropDown.style.border = "#555555 1px solid";
+        return;
+    }
+    closeDropDown();
+})
+
+var searchButton = document.querySelector('#search-button');
+var search = document.querySelector('#search')
+search.addEventListener('focus', function () {
+    searchButton.style.display = 'flex';
+})
+search.addEventListener('blur', function () {
+    searchButton.style.display = 'none';
+    search.value = '';
+})
